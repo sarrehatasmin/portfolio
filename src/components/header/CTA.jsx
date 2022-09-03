b3cbaf278849b3bb2092a63bf5f779f8f73b6d3c
Nitish Biswas <55139122+NitishBiswas@users.noreply.github.com>
2022-09-03 20:12:51 +0600

    Hello Rikta

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import CV from "../../assets/Resume.pdf";
+
+const CTA = () => {
+  return (
+    <div className="cta">
+      <a className="btn" href={CV} download="Resume of Rikta.pdf">
+        Download CV
+      </a>
+      <a className="btn btn-primary" href="#contact">
+        Let's Talk
+      </a>
+    </div>
+  );
+};
+
+export default CTA;
